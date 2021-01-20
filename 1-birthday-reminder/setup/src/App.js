c57a8369f90b5129dee510ca20e41e4879282f1e
vjreibo <valeriej1220@gmail.com>
2021-01-20 01:13:26 -0500

    birthday-reminder finished

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
   <main>
     <section className="container">
-      <h3>{people.length} birthdays today</h3>
+      <h2>{people.length} birthdays today</h2>
       <List people={people}/>
       <button onClick={()=>setPeople([])}>Clear All</button>
     </section>
