17b66bf91351f792fba8ad2ec81e510babc02414
vjreibo <valeriej1220@gmail.com>
2021-02-07 01:57:16 -0500

    lorem-ipsum finished

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,43 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [paragraphs,setParagraphs] = useState([]);
+  const [count,setCount] = useState(0);
   
-  
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    let temp = count;
+    if(count <= 0){
+      temp = 0;
+    }
+    else if(count > 8){
+      temp = 8;
+    }
+    setParagraphs(data.slice(0,temp));
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
+      <section className='section-center'>
+        <h3>Tired?</h3>
+        <form className='lorem-form' onSubmit={handleSubmit}>
+          <label labelFor='amount'>paragraphs: </label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={count}
+            onChange={(e)=>setCount(e.target.value)}
+          >
+          </input>
+          <button className='btn'>generate</button>
+        </form>
+        <article className='lorem-text'>
+        {paragraphs.map((paragraph,index)=>{
+            return <p key={index}>{paragraph}</p>;
+          })}
+        </article>
+        
+      </section>
     )
 }
 
